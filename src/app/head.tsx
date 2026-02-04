fad41414599b97a9320563269439e939b174640c
remon931116 <GitHub>
2026-02-05 05:14:45 +0800

    add head.tsx verification

---
+++ src/app/head.tsx
@@ -0,0 +1,10 @@
+export default function Head() {
+  return (
+    <>
+      <meta
+        name="google-site-verification"
+        content="xH5oJL_Xqj3LipkIS-1bIA30k4TDKRNKm44b1sOGfVI"
+      />
+    </>
+  );
+}
